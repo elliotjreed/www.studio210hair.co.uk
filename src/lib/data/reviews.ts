b7702e5287827136122e1a0238e24ca5a8d05246
Elliot J. Reed <git@elliotjreed.com>
2026-07-15 20:20:10 +0100

    Adds reviews from Treatwell

--- src/lib/data/reviews.ts
+++ src/lib/data/reviews.ts
@@ -56,5 +56,288 @@ export const reviews: Review[] = [
 		reviewerName: 'Samantha',
 		reviewText: "Fantastic. I wouldn't go anywhere else.",
 		rating: 5
+	},
+	{
+		reviewerName: 'Sara',
+		reviewText:
+			'Bridge is a lovely lady and very committed to make you feel your best. She did a great job with my hair and make up for a very special night and I could not recommend her enough.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Sarah',
+		reviewText: 'A friendly place and a great haircut',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText:
+			'always have the best experience always feel amazing when I come out of the salon x',
+		rating: 5
+	},
+	{
+		reviewerName: 'Natasha',
+		reviewText:
+			"I am in absolute in love with my hair I can't thank Bridget enough she did a amazing job on my hair definitely going back again to get it more blonder",
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerry',
+		reviewText:
+			'Thank you you have done an amazing job as usual see you soon p.s I wont leave it as long next time',
+		rating: 5
+	},
+	{
+		reviewerName: 'Julie',
+		reviewText:
+			'Another fantastic job on my hair. Love the colour and Brigid was her usual brilliant self making sure it was right for me. Thank you',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText:
+			'always come out feeling fantastic hair feels amazing, bridge is very knowledge and always have a great experience with her x',
+		rating: 5
+	},
+	{
+		reviewerName: 'Audra',
+		reviewText:
+			'Bride was lovely and friendly, only had a dry trim but cut was brilliant, made a massive difference to my hair',
+		rating: 5
+	},
+	{
+		reviewerName: 'Anthony',
+		reviewText:
+			'Always friendly and welcoming and cuts my hair just as I want. You can tell Brigid is very skilful as an hairdresser.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Tammy',
+		reviewText: 'Wash cut and blow dry',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText:
+			'always have a fantastic experience with bridge, my hair always feels amazing and bridge always styles my hair just how I want it. excellent experience',
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerry',
+		reviewText: 'Amazing as usuall sorry for being a grinch lol',
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerry',
+		reviewText: 'Brige was lovely as per usual love coming here',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText: 'always excellent experience, would highly recommend',
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerry',
+		reviewText: 'Loved the experience with the salon owner shes lovely',
+		rating: 5
+	},
+	{
+		reviewerName: 'Lisa',
+		reviewText:
+			"Lovely salon!! The ladies are fab and so lovely! First time coming here couldn't have asked for a better service. My daughter loved her hair and felt very comfortable! We've definitely found a new hairdressers.",
+		rating: 5
+	},
+	{
+		reviewerName: 'Helen',
+		reviewText:
+			'Was very pleased with my cut & blow from Natasha, who was friendly and thorough and added a few extra touches as I was going to a wedding. Thank you!',
+		rating: 5
+	},
+	{
+		reviewerName: 'Tiarnna',
+		reviewText:
+			'Really good stylist, welcoming and pays attention to detail. Gave me exactly what I asked for',
+		rating: 5
+	},
+	{
+		reviewerName: 'Stacey',
+		reviewText: 'Thank you to Natasha , made me feel comfortable and a good trim',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText: 'Always a great service, the ladies always make you feel very welcome',
+		rating: 5
+	},
+	{
+		reviewerName: 'Margaret',
+		reviewText: 'Lovely cut and blow dry, nice cuppa and chat too xx',
+		rating: 5
+	},
+	{
+		reviewerName: 'Tracey',
+		reviewText: 'Tash was friendly skilled and enjoyed having my high lights done there',
+		rating: 5
+	},
+	{
+		reviewerName: 'Sarah',
+		reviewText:
+			'Friendly staff, had a nice cup of coffee and a chat with my hair cut. It was worth travelling further than bulwell for it.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Laura',
+		reviewText:
+			'Felt welcome from the moment I entered the salon. Staff really friendly. Really happy with haircut.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerry',
+		reviewText: 'Tash was so lovey as ever and did a great job thank you',
+		rating: 5
+	},
+	{
+		reviewerName: 'Huma',
+		reviewText:
+			'Hair cut gave me a new look that complimented my face. Natasha was very professional and did a great job',
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerry',
+		reviewText: 'Mia is a real star and so lovely I will be back soon.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerry T',
+		reviewText:
+			'Lovely atmosphere both Bridget and Natasha were attentive kind. Natasha did my hair lovely (to say shes a novice at pixie cuts) highly recommend this salon and have booked for a facial Monday. Thank you ladies',
+		rating: 5
+	},
+	{
+		reviewerName: 'Sinead',
+		reviewText:
+			'Once again Brigid was amazing made sure my son was comfortable even made it funny for him if you are looking for someone who caters too children with additional needs this is your place too go',
+		rating: 5
+	},
+	{
+		reviewerName: 'Sinead',
+		reviewText: 'Was very accommodating towards my child was very happy with the results',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText: 'Whole experience great, hair looks fantastic',
+		rating: 5
+	},
+	{
+		reviewerName: 'Tina Frost',
+		reviewText: 'Lovely kind great hairdresser.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Julie',
+		reviewText:
+			"This is the 3rd time to the salon and I couldn't be happier. The staff are all so friendly and Brigid goes above and beyond to make sure you get exactly what you want and wont let you go until it is right. I'm 61 and I have to say this is the closest I have ever got to having my hair the colour I want. Thank you Brigid for your patience and attention to detail. Nothing was too much trouble. I shall certainly be a regular there from now on. A lovely place.",
+		rating: 5
+	},
+	{
+		reviewerName: 'Tiarnna',
+		reviewText:
+			'Laura did my hair, she was very chatty and friendly! She took great detail and care with my hair and when I asked for her opinion on what was best for my hair type and overall desired health she was honest and helpful. Overall really satisfied!!',
+		rating: 5
+	},
+	{
+		reviewerName: 'K. Williams',
+		reviewText: 'Lovely lady, great service and advice given. Will be back',
+		rating: 5
+	},
+	{
+		reviewerName: 'Lisa',
+		reviewText: 'Great venue in bus routes convenient to get to lovely staff',
+		rating: 5
+	},
+	{
+		reviewerName: 'Huma',
+		reviewText:
+			'Belinda was very professional and did a really good job, provided tips on after care as well.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Huma Khan',
+		reviewText:
+			'Both Belinda and Brigid have been extremely professional and friendly. They are there to make your whole experience amazing and you feel truly appreciated and listened to.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Nadia',
+		reviewText:
+			'Superb service as usual! Bridget did a fantastic job. Love the new salon! I was made to feel comfortable and at ease. Looks after my hair and explains the processes to ensure my hair is healthy. Hijaab friendly service, knows how to deal with Asian hair. Highly recommend. Warm friendly and professional service.',
+		rating: 5
+	},
+	{
+		reviewerName: 'Ayesha',
+		reviewText:
+			'Booked my daughter in for a haircut after not having great experiences elsewhere. We had Belinda as our advanced stylist & she was amazing, my five year old was wiggly and chatty but she was so lovely & patient with her and her hair looks stunning!',
+		rating: 5
+	},
+	{
+		reviewerName: 'Jac',
+		reviewText:
+			"One of the best professional haircuts I've had. Brigid was some welcoming and kind, and she has incredible talent. I will be making it my go to venue for haircuts going forward.",
+		rating: 5
+	},
+	{
+		reviewerName: 'Olga',
+		reviewText:
+			"If you want to look like a supermodel, come to this salon, and your dreams will come true!! My hair looks absolutely amazing, I love it! The day I come here is my best day!! The ladies are fantastic and honestly it's the best place in the country, the world!",
+		rating: 5
+	},
+	{
+		reviewerName: 'Marg',
+		reviewText:
+			'Belinda did a great job, she took her time and listened to what I wanted and I had a great cut and blow dry',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText: 'Fantastic service staff very friendly and welcoming',
+		rating: 5
+	},
+	{
+		reviewerName: 'Kerys',
+		reviewText: 'Perfect thank you x',
+		rating: 5
+	},
+	{
+		reviewerName: 'Molly',
+		reviewText: 'Exactly what I wanted! Very pleased and the best service :)',
+		rating: 5
+	},
+	{
+		reviewerName: 'Nikita',
+		reviewText:
+			"Really lovely women, made me feel welcome and comfortable the whole time, it was an absolute joy and I felt so much better after my hair cut. Can't wait to go back there to get my hair dyed.",
+		rating: 5
+	},
+	{
+		reviewerName: 'Catherine',
+		reviewText:
+			"Natasha is an absolute star. She was attentive, welcoming and just gave off an incredible aura. I didn't know really what I wanted doing to my hair and she gave perfect advice for my goals to keep the length. The whole service was 5 stars, and I have booked in again for my next hair cut! Give this new business a chance, you will not regret it! Thank you again Natasha!",
+		rating: 5
+	},
+	{
+		reviewerName: 'Beth',
+		reviewText: 'Very welcoming will definitely be going back',
+		rating: 5
+	},
+	{
+		reviewerName: 'Maria',
+		reviewText: 'All staff very friendly and welcoming lovely atmosphere',
+		rating: 5
+	},
+	{
+		reviewerName: 'Cadie',
+		reviewText: 'Great experience and love how my hair is thanks to my stylist.',
+		rating: 5
 	}
 ];
